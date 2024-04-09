03a3bd2aa909fe0c14226ba91266543997741847
Seth Lenhof <99466378+sethlenhof@users.noreply.github.com>
2024-04-08 20:28:07 -0400

    add apis and pages for setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+"use frontend";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/login.jsx";
+import Signup from "./pages/signup.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<main
+			style={{
+				padding: "0",
+				margin: "0",
+			}}>
+			<Background />
+			<Router>
+				<Routes>
+					<Route path="/" element={<Login />}></Route>
+					<Route path="/sign-up" element={<Signup />}></Route>
+				</Routes>
+			</Router>
+		</main>
+	);
 }
-
-export default App;
