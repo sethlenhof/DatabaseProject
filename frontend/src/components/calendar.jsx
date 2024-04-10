3257df8c335e8beab94a1112eb82998bf39bf538
Seth Lenhof <99466378+sethlenhof@users.noreply.github.com>
2024-04-10 02:08:14 -0400

    add calendar initial stuff

---
+++ frontend/src/components/calendar.jsx
@@ -0,0 +1,21 @@
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import listPlugin from '@fullcalendar/list';
+
+export default function Calendar({ events }) {
+    return (
+        <>
+        // Render the calendar
+        <FullCalendar
+            plugins={[dayGridPlugin]}
+            initialView="dayGridMonth"
+            events={events}
+        />
+        <FullCalendar
+            plugins={[listPlugin]}
+            initialView="listWeek"
+            events={events}    
+        />
+        </>
+    );
+}
--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import Calendar from '../components/calendar';
 
 export default class Landing extends React.Component {
+    // Define your events array
+    events=[
+        { title: 'event 1', date: '2024-04-01' },
+        { title: 'event 2', date: '2024-04-02' }
+    ]
+
     render() {
         return (
             <div>
-                <h1>Hello, World!</h1>
+                <Calendar events={this.events} /> {/* Use this.events to access the events array */}
+                <div>
+                    <h1>Hello, World!</h1>
+                </div>
             </div>
         );
     }
