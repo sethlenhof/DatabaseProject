f54b9a0e5562906849e14d94cf42f834cc7c1f0b
sethlenhof <setleelen@gmail.com>
2024-04-10 20:02:11 -0400

    test data

--- frontend/src/components/calendar2.jsx
+++ frontend/src/components/calendar2.jsx
@@ -5,8 +5,8 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import '../styles/calendar.css';
 
-export default class MyCalendar extends React.Component {
-    render() {
+export default function MyCalendar({events}) {
+    // render() {
       return (
             <FullCalendar
             plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
@@ -17,19 +17,22 @@ export default class MyCalendar extends React.Component {
                 center: 'title',
                 right: 'dayGridMonth,timeGridWeek,timeGridDay'
             }}
-            events={[
-                { 
-                title: 'Event 1',
-                start: '2024-04-10T10:00:00', // Specify the start time
-                end: '2024-04-10T12:00:00', // Specify the end time
-                color: '#ff9f89' // Customize the event color
-                },
-                // Add more events here
-            ]}
+            events={
+            //     [
+            //     { 
+            //     title: 'Event 1',
+            //     start: '2024-04-10T10:00:00', // Specify the start time
+            //     end: '2024-04-10T12:00:00', // Specify the end time
+            //     color: '#ff9f89' // Customize the event color
+            //     },
+            //     // Add more events here
+            // ]
+            events
+        }
             // Enable some interaction features:
             selectable={true}
             editable={true}
             />
       );
-    }
+    // }
   }
--- frontend/src/pages/test.jsx
+++ frontend/src/pages/test.jsx
@@ -6,15 +6,44 @@ import MyCalendar from '../components/calendar2';
 export default class Test extends React.Component {
     // Define your events array
     events=[
-        { title: 'event 1', date: '2024-04-01' },
-        { title: 'event 2', date: '2024-04-02' }
+        { title: 'Fucking pistachio', date: '2024-04-01' },
+        // { title: 'event 2', date: '2024-04-02' },
+        {title: 'Goon',
+            start: '2024-04-10T10:00:00', // Specify the start time
+            end: '2024-04-10T12:00:00', // Specify the end time
+            color: '#ff9f89' // Customize the event color
+                },
+            {title: 'test day',
+            date: '2024-04-13', // Specify the start time
+            color: '#ff9f89' // Customize the event color
+            },
+            {title: 'test',
+            start: '2024-04-13T12:00:00', // Specify the start time
+            end: '2024-04-13T24:00:00', // Specify the end time
+            color: 'red' // Customize the event color
+                },
+                {title: 'test',
+            start: '2024-04-13T14:00:00', // Specify the start time
+            end: '2024-04-13T16:00:00', // Specify the end time
+            color: 'blue' // Customize the event color
+                },
+                {title: 'test',
+            start: '2024-04-13T15:00:00', // Specify the start time
+            end: '2024-04-13T1:00:00', // Specify the end time
+            color: 'blue' // Customize the event color
+                },
+                {title: 'test',
+            start: '2024-04-13T15:00:00', // Specify the start time
+            end: '2024-04-13T1:00:00', // Specify the end time
+            color: 'red' // Customize the event color
+                }
     ]
 
     render() {
         return (
             <div>
                 {/* <Calendar events={this.events} /> */}
-                <MyCalendar />
+                <MyCalendar events={this.events}/>
             </div>
         );
     }
