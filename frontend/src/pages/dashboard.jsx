70f7e55f1e5a10abdbc96ad09fff9bd7ba748d21
sethlenhof <setleelen@gmail.com>
2024-04-15 17:28:25 -0400

    add api to get userType

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -4,12 +4,44 @@ import NavCluster from '../components/navCluster';
 import ListView from '../components/listView';
 
 export default class Dashboard extends React.Component {
-    // Define your events array
-    events=[
-        // ways to format the data
-        {title: 'Goon', start: '2024-04-10T10:00:00', end: '2024-04-10T12:00:00', color: '#ff9f89'},
-        {title: 'test day', date: '2024-04-13', color: '#ff9f89'},
-    ]
+    state = {
+        // Define your state here
+        userType: '',
+        isLoading: true,
+        userId: localStorage.getItem("user"),
+
+        // Define your events array
+        events: [
+            // ways to format the data
+            {title: 'Goon', start: '2024-04-10T10:00:00', end: '2024-04-10T12:00:00', color: '#ff9f89'},
+            {title: 'test day', date: '2024-04-13', color: '#ff9f89'},
+        ]
+    };
+
+    componentDidMount() {
+        
+    }
+
+    //fetch userType
+    async fetchUserType() {
+        fetch("/api/users/type", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                userId: this.state.userId,
+            }),
+        }).then((data) => {
+            if (data.status === 200) {
+                data.json().then((data) => {
+                    this.setState({ userType: data.userType, isLoading: false});
+                });
+            } else {
+                console.log("Error fetching user type");
+            }
+        });
+    }
 
     render() {
         return (
@@ -19,7 +51,7 @@ export default class Dashboard extends React.Component {
                 height: '100vh',
                 overflow: 'hidden',
             }}>
-                <NavCluster user={{ role: 'superAdmin' }} />
+                <NavCluster user={{ role: this.state.userType }} />
                 <div style={{
                         textAlign: 'center',
                     }}>
