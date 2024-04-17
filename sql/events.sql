39eb05be7ba245477c1efd4343bf28b997efb2a0
sethlenhof <setleelen@gmail.com>
2024-04-17 19:53:22 -0400

    fix event approval

--- sql/events.sql
+++ sql/events.sql
@@ -5,7 +5,7 @@ DROP PROCEDURE IF EXISTS get_unapproved_events;
 -- procedure to get unapproved events
 DELIMITER //
 
-CREATE PROCEDURE get_unapproved_events_for_university(IN input_user_id CHAR(255))
+CREATE PROCEDURE get_unapproved_events(IN input_user_id CHAR(255))
 BEGIN
     DECLARE uni_id INT;
 
@@ -18,28 +18,28 @@ BEGIN
     LEFT JOIN APPROVED_EVENTS ae ON e.EVENT_ID = ae.EVENT_ID
     WHERE ae.EVENT_ID IS NULL AND e.UNIVERSITY_ID = uni_id;
 END //
-
 DELIMITER ;
 
-DROP PROCEDURE IF EXISTS test_get_unapproved_events_for_university;
+DROP PROCEDURE IF EXISTS test_get_unapproved_events;
 
 -- test procedure for get_unapproved_events
-CREATE PROCEDURE test_get_unapproved_events_for_university()
+DELIMITER //
+CREATE PROCEDURE test_get_unapproved_events()
 BEGIN
-    DECLARE user_id CHAR(255);
-    SELECT INTO user_id USER_ID FROM SUPER_ADMIN WHERE EMAIL = "admin@ucf.edu";
-    CALL get_unapproved_events_for_university(user_id);
-END;
+    DECLARE userID CHAR(255);
+    SELECT USER_ID INTO userID FROM USER_LOGIN WHERE EMAIL = 'admin@ucf.edu';
+    CALL get_unapproved_events(userID);
+END //
+DELIMITER ;
 
+CALL test_get_unapproved_events();
 
 DROP PROCEDURE IF EXISTS approve_event;
--- procedure to approve an event
 DELIMITER //
 CREATE PROCEDURE approve_event(IN input_super_admin_id CHAR(255), IN input_event_id INT)
 BEGIN
     DECLARE uni_id INT;
     DECLARE event_uni_id INT;
-
     -- Get the university ID associated with the super admin
     SELECT UNIVERSITY_ID INTO uni_id FROM SUPER_ADMIN WHERE USER_ID = input_super_admin_id;
 
@@ -54,20 +54,26 @@ BEGIN
 
         SELECT 'Success' AS Status, 'Event approved successfully.' AS Message;
     ELSE
-        SELECT 'Error' AS Status, 'Event cannot be approved by this super admin.' AS Message;
+        SELECT 'Error' AS Status, 'Event cannot be approved by this super admin, mismatch in university IDs.' AS Message;
     END IF;
 END //
-
 DELIMITER ;
 
+
 DROP PROCEDURE IF EXISTS test_approve_event;
--- test procedure for approve_event
+
+-- Test procedure for approve_event
+DELIMITER //
 CREATE PROCEDURE test_approve_event()
 BEGIN
     DECLARE super_admin_id CHAR(255);
+    DECLARE uni_id INT;
     DECLARE event_id INT;
-    SELECT INTO super_admin_id USER_ID FROM SUPER_ADMIN WHERE EMAIL = "admin@ucf.edu";
-    SELECT INTO event_id EVENT_ID FROM EVENTS WHERE UNIVERSITY_ID = 1;
-    CALL approve_event(super_admin_id, event_id);
-END;
+    DECLARE test_uni_id INT;
+    SELECT USER_ID INTO super_admin_id FROM USER_LOGIN WHERE EMAIL = 'admin@ucf.edu';
+    SELECT UNIVERSITY_ID INTO uni_id FROM SUPER_ADMIN WHERE USER_ID = super_admin_id;
+    CALL approve_event(super_admin_id, 1);
+END //
 DELIMITER ;
+
+CALL test_approve_event();
