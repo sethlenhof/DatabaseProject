f20961250070c07f9e9a25d2e680b12f7101fbe4
sethlenhof <setleelen@gmail.com>
2024-04-17 02:57:58 -0400

    update with stuff

--- sql/createRSOandAdmin.sql
+++ sql/createRSOandAdmin.sql
@@ -11,7 +11,7 @@ DROP PROCEDURE IF EXISTS testRSO;
 DELIMITER //
 CREATE PROCEDURE create_rso_and_admin(
     IN input_user_id CHAR(255),
-    IN rso_name VARCHAR(255),
+    IN input_rso_name VARCHAR(255),
     IN rso_color VARCHAR(255),
     IN rso_description TEXT
 )
@@ -27,7 +27,7 @@ BEGIN
     );
     
     -- Check if an RSO with the same name already exists
-    SELECT COUNT(*) INTO existing_rso_count FROM RSO WHERE RSO_NAME = rso_name;
+    SELECT COUNT(*) INTO existing_rso_count FROM RSO WHERE RSO_NAME = input_rso_name;
 
     -- Start the transaction here to ensure all following operations are atomic
     START TRANSACTION;
@@ -43,7 +43,7 @@ BEGIN
     ELSE
         -- Insert new RSO
         INSERT INTO RSO (RSO_NAME, COLOR, RSO_DESCRIPTION)
-        VALUES (rso_name, rso_color, rso_description);
+        VALUES (input_rso_name, rso_color, rso_description);
 
         -- Capture the RSO_ID of the newly created RSO
         SET new_rso_id = LAST_INSERT_ID();
@@ -82,6 +82,11 @@ DELIMITER //
     DECLARE userID CHAR(255);
     SELECT USER_ID INTO userID FROM USER_LOGIN WHERE EMAIL = 'admin@admin.com';
     CALL create_rso_and_admin(userID, 'Sample RSO', 'red', 'RSO Description');
+    CALL create_rso_and_admin(userID, 'UCF CLUB1', 'red', 'RSO Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam commodo, turpis at venenatis facilisis, ex dolor dictum nunc, eu varius arcu orci non massa. Donec tincidunt suscipit finibus. Vestibulum sed nisl cursus, pellentesque lorem in, maximus turpis. Morbi fringilla mauris tempor, sodales turpis non, auctor mauris. Quisque sed vulputate dui. Sed non dapibus sapien, sit amet viverra velit. Sed ultrices sem vel lectus pretium, et porta eros tincidunt. Suspendisse facilisis nibh urna, id aliquet sem interdum eu. Suspendisse pulvinar ex eget lacinia aliquam. Sed ultricies suscipit consequat. Etiam scelerisque vehicula vehicula. Praesent ut dolor ex. Cras dictum vel nunc quis accumsan. Sed sed volutpat urna');
+    CALL create_rso_and_admin(userID, 'UCF CLUB2', 'pink', 'RSO Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam commodo, turpis at venenatis facilisis, ex dolor dictum nunc, eu varius arcu orci non massa. Donec tincidunt suscipit finibus. Vestibulum sed nisl cursus, pellentesque lorem in, maximus turpis. Morbi fringilla mauris tempor, sodales turpis non, auctor mauris. Quisque sed vulputate dui. Sed non dapibus sapien, sit amet viverra velit. Sed ultrices sem vel lectus pretium, et porta eros tincidunt. Suspendisse facilisis nibh urna, id aliquet sem interdum eu. Suspendisse pulvinar ex eget lacinia aliquam. Sed ultricies suscipit consequat. Etiam scelerisque vehicula vehicula. Praesent ut dolor ex. Cras dictum vel nunc quis accumsan. Sed sed volutpat urna');
+    CALL create_rso_and_admin(userID, 'UCF CLUB3', 'orange', 'RSO Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam commodo, turpis at venenatis facilisis, ex dolor dictum nunc, eu varius arcu orci non massa. Donec tincidunt suscipit finibus. Vestibulum sed nisl cursus, pellentesque lorem in, maximus turpis. Morbi fringilla mauris tempor, sodales turpis non, auctor mauris. Quisque sed vulputate dui. Sed non dapibus sapien, sit amet viverra velit. Sed ultrices sem vel lectus pretium, et porta eros tincidunt. Suspendisse facilisis nibh urna, id aliquet sem interdum eu. Suspendisse pulvinar ex eget lacinia aliquam. Sed ultricies suscipit consequat. Etiam scelerisque vehicula vehicula. Praesent ut dolor ex. Cras dictum vel nunc quis accumsan. Sed sed volutpat urna');
+    CALL create_rso_and_admin(userID, 'UCF CLUB4', 'green', 'RSO Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam commodo, turpis at venenatis facilisis, ex dolor dictum nunc, eu varius arcu orci non massa. Donec tincidunt suscipit finibus. Vestibulum sed nisl cursus, pellentesque lorem in, maximus turpis. Morbi fringilla mauris tempor, sodales turpis non, auctor mauris. Quisque sed vulputate dui. Sed non dapibus sapien, sit amet viverra velit. Sed ultrices sem vel lectus pretium, et porta eros tincidunt. Suspendisse facilisis nibh urna, id aliquet sem interdum eu. Suspendisse pulvinar ex eget lacinia aliquam. Sed ultricies suscipit consequat. Etiam scelerisque vehicula vehicula. Praesent ut dolor ex. Cras dictum vel nunc quis accumsan. Sed sed volutpat urna');
+    CALL create_rso_and_admin(userID, 'UCF CLUB5', 'black', 'RSO Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam commodo, turpis at venenatis facilisis, ex dolor dictum nunc, eu varius arcu orci non massa. Donec tincidunt suscipit finibus. Vestibulum sed nisl cursus, pellentesque lorem in, maximus turpis. Morbi fringilla mauris tempor, sodales turpis non, auctor mauris. Quisque sed vulputate dui. Sed non dapibus sapien, sit amet viverra velit. Sed ultrices sem vel lectus pretium, et porta eros tincidunt. Suspendisse facilisis nibh urna, id aliquet sem interdum eu. Suspendisse pulvinar ex eget lacinia aliquam. Sed ultricies suscipit consequat. Etiam scelerisque vehicula vehicula. Praesent ut dolor ex. Cras dictum vel nunc quis accumsan. Sed sed volutpat urna');
 
     SELECT * FROM RSO;
     SELECT * FROM RSO_ADMIN;
